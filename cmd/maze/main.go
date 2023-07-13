0ba23edc71538618ca247339e619bd8c03a2843e
Luke Orth <lorth001@gmail.com>
2023-07-13 10:39:09 -0500

    Adding generic backtrack code

--- cmd/maze/main.go
+++ cmd/maze/main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"os"
 
 	"github.com/lukeorth/maze"
 )
 
-func main() {
-    m := maze.NewMaze(1000, 1000)
+var (
+    width int
+    height int
+    scale int
+    out string
+)
 
-    f, _ := os.Create("test.png")
+func init() {
+    flag.IntVar(&width, "w", 10, "maze width")
+    flag.IntVar(&height, "h", 10, "maze height")
+    flag.IntVar(&scale, "s", 1, "maze scale (number of pixels per cell)")
+    flag.StringVar(&out, "o", "maze.png", "output image path")
+}
+
+func main() {
+    // gracefully handle errors and exit
+    var err error
+    defer func() {
+        if err != nil {
+            log.Fatalln(err)
+        }
+    }()
+
+    flag.Parse()
+
+    m := maze.NewMaze(width, height)
+
+    f, _ := os.Create(out)
     defer f.Close()
 
-    m.Png(f, 5)
+    m.Png(f, scale)
 }
